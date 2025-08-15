c993e9d1d5c9bb33985bd35e4170b50c5222b462
HashirHexer8 <hashir@hexerdev.com>
2025-08-15 19:21:37 +0500

    update base url

--- app/src/main/java/com/example/monitorapp/network/NetworkConstants.kt
+++ app/src/main/java/com/example/monitorapp/network/NetworkConstants.kt
@@ -1,8 +1,9 @@
 package com.example.monitorapp.network
 
 object NetworkConstants {
-    const val BASE_URL = "http://192.168.18.240:3005" // TODO: Replace with your actual server IP
-    //const val SOCKET_HOST = "wss://monitorappbe-production.up.railway.app"
-    const val SOCKET_HOST = "ws://192.168.18.240:3005"
+    //const val BASE_URL = "http://192.168.100.176:3005" // TODO: Replace with your actual server IP
+    const val BASE_URL = "http://monitorappbe-production.up.railway.app"
+    const val SOCKET_HOST = "wss://monitorappbe-production.up.railway.app"
+    //const val SOCKET_HOST = "ws://192.168.100.176:3005"
     const val REGISTER_DEVICE_ENDPOINT = "/register-device"
 } 
